cc18ce4a42eefeb4b4c67de14c7fd93b38d46f1a
David Maydan <dmaydan2@gmail.com>
2018-07-07 22:29:48 -0400

    update

--- filldatabase.py
+++ filldatabase.py
@@ -1,11 +1,18 @@
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-from database_setup import Base, Category, Item, User
+from app import Category, Item, User
 
 engine = create_engine("""[db_name]""")
 # Bind the engine to the metadata of the Base class so that the
 # declaratives can be accessed through a DBSession instance
+
+Base = declarative_base()
+
+
 Base.metadata.bind = engine
 
 DBSession = sessionmaker(bind=engine)
